5807bb5452df59e0bfe697bf8c98b38b9e9e3243
Alyssa Kwan <alyssa.kwan@cloverhealth.com>
2015-12-09 14:53:13 -0800

    01 - object lifecycle - only 2 words

--- 01_object_lifecycle/object_lifecycle.py
+++ 01_object_lifecycle/object_lifecycle.py
@@ -1,60 +1,79 @@
 import collections
-import datetime
+import re
+
+
+def nested_defaultdict():
+    return collections.defaultdict(nested_defaultdict)
 
 
 class WordSequence(object):
 
-    def __init__(self, word0, word1, word2):
-        self._value = (word0, word1, word2)
+    def __init__(self, word0, word1):
+        self.value = (word0, word1)
+
+    def __eq__(self, other):
+        return (self is other
+                or self.value is other.value
+                or self.value == other.value
+        )
+
+    def __hash__(self):
+        return hash(self.value)
 
 
 class FlyweightWordSequence(object):
 
-    cache = {}
+    cache = nested_defaultdict()
 
-    def __new__(cls, word0, word1, word2):
-        word_cache = cls.cache
-        if not word0 in word_cache:
-            word_cache[word0] = {}
-        word_cache = word_cache[word0]
-        if not word1 in word_cache:
-            word_cache[word1] = {}
-        word_cache = word_cache[word1]
-        if not word2 in word_cache:
-            word_cache[word2] = (word0, word1, word2)
-        return word_cache[word2]
+    def __new__(cls, word0, word1):
+        if not word1 in cls.cache[word0]:
+            cls.cache[word0][word1] = word_sequence = super().__new__(cls)
+            word_sequence.value = (word0, word1)
+        return cls.cache[word0][word1]
 
-    @property
-    def value(self):
-        return self
+    def __eq__(self, other):
+        return (self is other
+                or self.value is other.value
+                or self.value == other.value
+        )
+
+    def __hash__(self):
+        return hash(self.value)
 
 
 def count_word_sequences(path, class_):
     counter = collections.Counter()
+    word_pattern = re.compile(r"[\w']+")
     with open(path) as f:
         line = f.readline()
         words = []
         while line != '':
-            words += line.split(' ')
-            while len(words) >= 3:
-                word_sequence = class_(words[0], words[1], words[2])
+            words += word_pattern.findall(line)
+            while len(words) >= 2:
+                word_sequence = class_(words[0], words[1])
                 counter[word_sequence] += 1
-                words = words[1:]
+                words.pop(0)
             line = f.readline()
-    return len(counter)
+    output = {'len': len(counter), 'top_3': {
+        word_sequence.value: count
+        for word_sequence, count in counter.most_common(3)
+    }}
+    print(output)
+    return output
+
 
 def main():
-    print('WordSequence:')
-    before_time = datetime.datetime.now()
-    print('    return: ' + str(count_word_sequences('./war_and_peace.txt', WordSequence)))
-    after_time = datetime.datetime.now()
-    print('    time:   ' + str((after_time - before_time).microseconds))
-
-    print('FlyweightWordSequence:')
-    before_time = datetime.datetime.now()
-    print('    return: ' + str(count_word_sequences('./war_and_peace.txt', FlyweightWordSequence)))
-    after_time = datetime.datetime.now()
-    print('    time:   ' + str((after_time - before_time).microseconds))
+    import timeit
+    print('WordSequence: ' + str(timeit.timeit(
+        stmt="count_word_sequences('./war_and_peace.txt', WordSequence)",
+        setup='from __main__ import count_word_sequences, WordSequence',
+        number=1
+    )))
+    print('FlyweightWordSequence: ' + str(timeit.timeit(
+        stmt="count_word_sequences('./war_and_peace.txt', FlyweightWordSequence)",
+        setup='from __main__ import count_word_sequences, FlyweightWordSequence',
+        number=1
+    )))
 
 if __name__ == '__main__':
     main()
